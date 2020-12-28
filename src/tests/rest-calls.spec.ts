e4bb7c8629813e83b1142f77d002e247ed746c2d
Beata Kruzycka <beata.kruzycka@finastra.com>
2020-12-28 21:36:16 +0100

    test for rest calls

--- src/tests/rest-calls.spec.ts
+++ src/tests/rest-calls.spec.ts
@@ -18,40 +18,112 @@ describe('Check axios-har-tracker', () => {
       writeFileSync('example.har', JSON.stringify(getHar), 'utf-8')
     });
 
-    it('should get har from call which returns 200', async () => {
-      const res1 = await axios.get('http://httpstat.us/200');
-      console.log("DEBU res1:", res1)
+    it('should get har which includes status 200', async () => {
+      await axios.get('http://httpstat.us/200');
       getHar = axiosTracker.getGeneratedHar();
-      console.log("DEBUG getHar with 200",getHar)
+      const array = getHar.log.entries;
+      expect(array[0].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/200"
+      });
+      expect(array[0].response).toMatchObject({
+        "status": 200,
+        "statusText": "OK"
+      });
+      expect(array[1]).toBeUndefined();
     });
 
-    it('should get har from call which returns 300', async () => {
-      const res2 = await axios.get('http://httpstat.us/300');
-      console.log("DEBU res2:", res2)
+    it('should get har which includes status 302 and previous request with a response', async () => {
+      await axios.get('http://httpstat.us/302');
       getHar = axiosTracker.getGeneratedHar();
-      console.log("DEBUG getHar with 200 and 300",getHar)
-      // TODO
+      const array = getHar.log.entries;
+      expect(array[0].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/200"
+      });
+      expect(array[0].response).toMatchObject({
+        "status": 200,
+        "statusText": "OK"
+      });
+      expect(array[1].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/302"
+      });
+      expect(array[1].response).toMatchObject({
+        "status": 302,
+        "statusText": "Found"
+      });
+      expect(array[2]).toBeUndefined();
     });
 
-    it('should get har from call which returns 302', async () => {
-      const res3 = await axios.get('http://httpstat.us/302');
-      console.log("DEBU response:", res3)
+    it('should get har which includes status 404 and all previous requests with responses', async () => {
+      await axios.get('http://httpstat.us/302');
       getHar = axiosTracker.getGeneratedHar();
-      // TODO
+      const array = getHar.log.entries;
+      expect(array[0].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/200"
+      });
+      expect(array[0].response).toMatchObject({
+        "status": 200,
+        "statusText": "OK"
+      });
+      expect(array[1].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/302"
+      });
+      expect(array[1].response).toMatchObject({
+        "status": 302,
+        "statusText": "Found"
+      });
+      expect(array[2].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/404"
+      });
+      expect(array[2].response).toMatchObject({
+        "status": 404,
+        "statusText": "Not Found"
+      });
+      expect(array[3]).toBeUndefined();
     });
 
-    it('should get har from call which returns 404', async () => {
-      const res4 = await axios.get('http://httpstat.us/404');
-      console.log("DEBU response:", res4)
+    it('should get har which includes status 500 and all previous requests with responses', async () => {
+      await axios.get('http://httpstat.us/500');
       getHar = axiosTracker.getGeneratedHar();
-      // TODO
-    });
-
-    it('should get har from call which returns 500', async () => {
-      const res5 = await axios.get('http://httpstat.us/500');
-      console.log("DEBU response:", res5)
-      getHar = axiosTracker.getGeneratedHar();
-      // TODO
+      const array = getHar.log.entries;
+      expect(array[0].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/200"
+      });
+      expect(array[0].response).toMatchObject({
+        "status": 200,
+        "statusText": "OK"
+      });
+      expect(array[1].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/302"
+      });
+      expect(array[1].response).toMatchObject({
+        "status": 302,
+        "statusText": "Found"
+      });
+      expect(array[2].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/404"
+      });
+      expect(array[2].response).toMatchObject({
+        "status": 404,
+        "statusText": "Not Found"
+      });
+      expect(array[3].request).toMatchObject({
+        "method": "get",
+        "url": "http://httpstat.us/500"
+      });
+      expect(array[3].response).toMatchObject({
+        "status": 500,
+        "statusText": "Not Implemented"
+      });
+      expect(array[4]).toBeUndefined();
     });
 
 });
