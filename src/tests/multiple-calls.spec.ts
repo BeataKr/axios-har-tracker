f7b32cd3037b63cab99655c00186345200e682e7
Julian Rabe <jr@tough.dev>
2024-04-03 10:43:57 +0200

    chore(tests): let tests fail when preconditions are not met

--- src/tests/multiple-calls.spec.ts
+++ src/tests/multiple-calls.spec.ts
@@ -14,13 +14,9 @@ describe("axios-har-tracker e2e tests", () => {
   });
 
   it("Should collect call with status 200", async () => {
-    try {
-      await axios.get("http://httpstat.us/200");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/200"
-      );
-    }
+    await axios.get("http://httpstat.us/200", {
+      validateStatus: (status) => status === 200
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
@@ -37,14 +33,10 @@ describe("axios-har-tracker e2e tests", () => {
   });
 
   it("Should collect call to 302 - reject unauthorized", async () => {
-    try {
-      const response302 = await axios.get("http://httpstat.us/302");
-      console.log("DEBUG response302", response302);
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/302"
-      );
-    }
+    await axios.get("http://httpstat.us/302", {
+      maxRedirects: 0,
+      validateStatus: (status) => status === 302
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
@@ -57,13 +49,9 @@ describe("axios-har-tracker e2e tests", () => {
   });
 
   it("Should collect call with status 404", async () => {
-    try {
-      await axios.get("http://httpstat.us/404");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/404"
-      );
-    }
+    await axios.get("http://httpstat.us/404", {
+      validateStatus: (status) => status === 404
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
@@ -80,13 +68,9 @@ describe("axios-har-tracker e2e tests", () => {
   });
 
   it("Should collect call with status 500", async () => {
-    try {
-      await axios.get("http://httpstat.us/500");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/500"
-      );
-    }
+    await axios.get("http://httpstat.us/500", {
+      validateStatus: (status) => status === 500
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
@@ -103,28 +87,19 @@ describe("axios-har-tracker e2e tests", () => {
   });
 
   it("Should collect multiple calls", async () => {
-    await axios.get("http://httpstat.us/200");
-    try {
-      await axios.get("http://httpstat.us/302");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/302"
-      );
-    }
-    try {
-      await axios.get("http://httpstat.us/404");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to http://httpstat.us/404"
-      );
-    }
-    try {
-      await axios.get("http://httpstat.us/500");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to http://httpstat.us/500"
-      );
-    }
+    await axios.get("http://httpstat.us/200", {
+      validateStatus: (status) => status === 200
+    });
+    await axios.get("http://httpstat.us/302", {
+      maxRedirects: 0,
+      validateStatus: (status) => status === 302
+    });
+    await axios.get("http://httpstat.us/404", {
+      validateStatus: (status) => status === 404
+    });
+    await axios.get("http://httpstat.us/500", {
+      validateStatus: (status) => status === 500
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
@@ -161,19 +136,13 @@ describe("axios-har-tracker e2e tests", () => {
   }, 20000);
 
   it("Should collect call with query parameters", async () => {
-    try {
-      await axios.get("http://httpstat.us/200", {
-        params: {
-          test: 1,
-        },
-      });
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/200"
-      );
-    }
+    await axios.get("http://httpstat.us/200", {
+      params: {
+        test: 1,
+      },
+      validateStatus: (status) => status === 200
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
-    console.log(JSON.stringify(generatedHar, null, 2));
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
       method: "get",
@@ -201,15 +170,10 @@ describe("axios-har-tracker e2e tests", () => {
 
     axiosTracker = new AxiosHarTracker(axiosInstance);
 
-    try {
-      await axiosInstance.get("/200");
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/200"
-      );
-    }
+    await axiosInstance.get("/200", {
+      validateStatus: (status) => status === 200
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
-    console.log(JSON.stringify(generatedHar, null, 2));
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
       method: "get",
@@ -231,19 +195,13 @@ describe("axios-har-tracker e2e tests", () => {
 
     axiosTracker = new AxiosHarTracker(axiosInstance);
 
-    try {
-      await axiosInstance.get("/200", {
-        params: {
-          test: 1,
-        },
-      });
-    } catch (error) {
-      console.log(
-        "An expected error appears after call to https://httpstat.us/200"
-      );
-    }
+    await axiosInstance.get("/200", {
+      params: {
+        test: 1,
+      },
+      validateStatus: (status) => status === 200
+    });
     const generatedHar = axiosTracker.getGeneratedHar();
-    console.log(JSON.stringify(generatedHar, null, 2));
     const array = generatedHar.log.entries;
     expect(array[0].request).toMatchObject({
       method: "get",
